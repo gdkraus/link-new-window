02776e7dce37ed1fd20d692c6329742bebb7f7df
Greg Kraus <gdkraus@ncsu.edu>
2013-02-11 20:40:39 -0500

    added jQuery(document).ready()

--- link-new-window.js
+++ link-new-window.js
@@ -1,7 +1,10 @@
-// Add the class to all <a> elements with target="_blank"
-jQuery('a[target="_blank"]').addClass('ncsu-a11y-link-new-window');
+jQuery(document).ready(function(){
 
-// Add the icon to the end of the link text.
-// Be sure to adjust the src to point to the local version of the new window icon.
-jQuery('a[target="_blank"]').each(function(){jQuery(this).html(jQuery(this).html() + '<span><img src="new-window.png" alt="opens in a new window"></span>')});
-  
+	// Add the class to all <a> elements with target="_blank"
+	jQuery('a[target="_blank"]').addClass('ncsu-a11y-link-new-window');
+
+	// Add the icon to the end of the link text.
+	// Be sure to adjust the src to point to the local version of the new window icon.
+	jQuery('a[target="_blank"]').each(function(){jQuery(this).html(jQuery(this).html() + '<span><img src="new-window.png" alt="opens in a new window"></span>')});
+
+}
